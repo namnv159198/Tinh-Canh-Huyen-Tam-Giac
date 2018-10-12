e642d47c604ecdb9fbdd72e22bbffa32409cf5b8
namnvth1809002 <namnvth1809002@fpt.edu.vn>
2018-10-12 21:00:21 +0700

    Tinh canh huyen tam giac

---
+++ main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdbool.h>
+
+bool Canh(int a,int b){
+    if (a<=0 || b<=0){
+        return false;
+    }
+    return  true;
+}
+
+double Canhhuyen(int a,int b){
+    return  sqrt(a*a + b*b) ;
+}
+int main() {
+    int a,b;
+    printf("Tam giac vuong ABC: \n");
+    printf("Nhap canh A:  \n");
+    scanf("%d",&a);
+    printf("Nhap canh B:  \n");
+    scanf("%d",&b);
+    bool kt = Canh(a,b);
+    if(kt){
+        printf("Canh huyen C cua tam giac ABC la: %.2f \n",Canhhuyen(a,b));
+    }
+
+}
